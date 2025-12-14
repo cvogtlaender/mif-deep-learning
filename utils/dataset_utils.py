d14d48d6dca6e1d2c48d02b1329f22a70e180286
dgorpinic <gorpinich.d58@gmail.com>
2025-12-14 04:02:45 +0100

    add standard augmentation (horizontal flip) to train transformer

--- utils/dataset_utils.py
+++ utils/dataset_utils.py
@@ -3,19 +3,24 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 
 transform_train = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    # transforms.Resize(256),
+    # transforms.CenterCrop(224),
+    transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
     transforms.ToTensor(),
-    transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                         std=(0.229, 0.224, 0.225))
+    transforms.Normalize(
+        mean=(0.485, 0.456, 0.406),
+        std=(0.229, 0.224, 0.225),
+    ),
 ])
 
 transform_test = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize(mean=(0.485, 0.456, 0.406),
-                         std=(0.229, 0.224, 0.225))
+    transforms.Normalize(
+        mean=(0.485, 0.456, 0.406),
+        std=(0.229, 0.224, 0.225))
 ])
 
 # ---------------------------------------------------------------------------------------------------------------------
